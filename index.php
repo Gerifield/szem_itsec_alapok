9f8540ee9968b8547b4e26570cafb0cc7144a071
Gerifield <radics.gergely@ustream.tv>
2014-10-15 16:44:23 +0200

    Added index

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>IT Biztonság alapok</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+<p><a href="slides/index.html">Diasor</a></p>
+<ul>
+	<li><a href="a1-injection/">SQL Injection</a></li>
+	<li><a href="a2-auth/">Auth bugs</a></li>
+	<li><a href="a3-xss/">XSS</a></li>
+</ul>
+</body>
+</html>

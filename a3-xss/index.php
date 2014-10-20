fd1a50a0fb998195c5f308fc7f1b988ca3366e8a
Gerifield <radics.gergely@ustream.tv>
2014-10-20 10:45:12 +0200

    Added blog like stuff

--- a3-xss/index.php
+++ a3-xss/index.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=szem_db', 'root', '');
 ?>
 
 <!DOCTYPE html>
@@ -13,9 +15,53 @@ session_start();
 
 <?php
 if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
+
+	//Actions
+	if(isset($_GET['a'])){
+		switch ($_GET['a']) {
+			case 'logout':
+				session_destroy();
+				header('location: index.php');
+				break;
+			
+			case 'post':
+				if(!empty($_POST['title']) && !empty($_POST['body'])){
+					$stmt = $pdo->prepare("INSERT INTO post (user_id, title, body) VALUES (?, ?, ?)");
+					$stmt->execute(array($_SESSION['user_id'], $_POST['title'], $_POST['body']));
+					header('location: index.php');
+				}else{
+					$errorMsg = "Hiányzó paraméter!";
+				}
+				break;
+		}
+	}
 ?>
 
+<a href="?a=logout">Kilépés</a>
 
+<?php if(isset($errorMsg)): ?>
+	<br /><font color="red"><b><?php echo $errorMsg; ?></b></font><br /><br />
+<?php endif; ?>
+
+<table>
+<form method="post" action="?a=post">
+	<tr><td>Cím</td> <td><input type="text" name="title"></td></tr>
+	<tr><td>Tartalom</td><td><textarea name="body" rows="5" cols="40"></textarea></td></tr>
+	<tr><td colspan="2"><input type="submit" value="Küldés"></td></tr>
+</form>
+</table>
+<br />
+
+<?php
+	$stmt = $pdo->prepare("SELECT * FROM post INNER JOIN user ON user.id = post.user_id ORDER BY post.id DESC");
+	$stmt->execute();
+	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
+
+<hr />
+Cím: <?php echo $row['title']; ?><br />
+Tartalom: <?php echo $row['body']; ?><br />
+Tulaj: <?php echo $row['name']; ?><br />
+<?php endwhile; ?>
 
 
 <?php
@@ -26,7 +72,7 @@ if(isset($_POST['user'])){
 	//print_r($_POST);
 	//echo "</pre>";
 
-	$pdo = new PDO('mysql:host=127.0.0.1;dbname=szem_db', 'root', '');
+	
 	$stmt = $pdo->prepare("SELECT * FROM user WHERE name = ? AND password = ?");
 	$stmt->bindParam(1, $_POST['user'], PDO::PARAM_STR);
 	$stmt->bindParam(2, md5($_POST['password']), PDO::PARAM_STR);
@@ -35,12 +81,14 @@ if(isset($_POST['user'])){
 
 	if($stmt->rowCount() > 0){
 		$userData = $stmt->fetch(PDO::FETCH_ASSOC);
-		echo "<font color='green'>OK!</font>";
-		echo "<pre>";
-		print_r($userData);
-		echo "</pre>";
+		//echo "<font color='green'>OK!</font>";
+		//echo "<pre>";
+		//print_r($userData);
+		//echo "</pre>";
 		$_SESSION['loggedin'] = true;
 		$_SESSION['user'] = $userData['name'];
+		$_SESSION['user_id'] = $userData['id'];
+		header('location: index.php');
 	}else{
 		echo "<font color='red'>Error!</font>";
 	}

